b19f1ee4febfff460658c7d04505715c206a1e95
YourName <your-email@example.com>
2026-02-08 00:59:04 +0530

    update html and js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-// function toggleMenu() {
-//      const menu = document.querySelector(".menu-links"); 
-//      const icon = document.querySelector(".hamburger-icon"); 
-//       menu.classList.toggle("open");
-//       icon.classList.toggle("open");
-//     }
-let cont = function toggleMenu() {
-  console.log("Menu clicked");
-}
+function toggleMenu() {
+     const menu = document.querySelector(".menu-links"); 
+     const icon = document.querySelector(".hamburger-icon"); 
+      menu.classList.toggle("open");
+      icon.classList.toggle("open");
+    }
 
-console.log("cont");

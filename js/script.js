de0565876deb06a44a8b89da1534eb89df3e5df9
YourName <your-email@example.com>
2026-02-08 00:49:16 +0530

    add css and js structure

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// function toggleMenu() {
+//      const menu = document.querySelector(".menu-links"); 
+//      const icon = document.querySelector(".hamburger-icon"); 
+//       menu.classList.toggle("open");
+//       icon.classList.toggle("open");
+//     }
+let cont = function toggleMenu() {
+  console.log("Menu clicked");
+}
+
+console.log("cont");

5a8db9bac2e9c102d28f77bfabcfffe1cd5e3590
YourName <your-email@example.com>
2026-02-12 21:16:47 +0530

    Update about section, styles and portfolio images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 function toggleMenu() {
-     const menu = document.querySelector(".menu-links"); 
-     const icon = document.querySelector(".hamburger-icon"); 
-      menu.classList.toggle("open");
-      icon.classList.toggle("open");
-    }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleBtn.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
